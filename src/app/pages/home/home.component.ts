27d72de19db14d82c9dacfb2b1b3b389fd54331f
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-09-28 00:46:39 +0300

    added marcup

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,14 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
+
 })
 export class HomeComponent implements OnInit {
+
+  cols = 3;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onColumsCountChange(colsNum: number): void {
+
+    this.cols = colsNum
+  }
 }
