bb0ac607aeff71a892fcebf6292137bfc571b5f9
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-10-04 22:25:57 +0300

    added logic to buttons

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -34,4 +34,36 @@ export class CartService {
     this._snackBar.open(" Cart is clear", "OK", { duration: 3000 })
 
   }
+
+  removedFromCart(item: CartItem, update = true): Array<CartItem> {
+    const filterItems = this.cart.value.items.filter((_item) => _item.id !== item.id)
+
+    if (update) {
+      this.cart.next({ items: filterItems })
+      this._snackBar.open("1 Item removed from cart", "Ok", { duration: 3000 })
+
+    }
+
+    return filterItems;
+  }
+
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval: CartItem | undefined
+    let filteredItems = this.cart.value.items.map((_item) => {
+      if (_item.id === item.id) {
+        _item.quantity -= 1;
+        if (_item.quantity === 0) {
+          itemForRemoval = _item
+        }
+      }
+      return _item
+    })
+
+    if (itemForRemoval) {
+      filteredItems = this.removedFromCart(itemForRemoval, false)
+    }
+    this.cart.next({ items: filteredItems })
+    this._snackBar.open('1 item removed from cart', 'Ok', { duration: 3000 })
+  }
+
 }
