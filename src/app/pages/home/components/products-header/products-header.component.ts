9506d0219d0668329ab42af2b71c6aa7f422bb27
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-10-06 16:10:49 +0300

    added sort and count products

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -6,7 +6,10 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 })
 export class ProductsHeaderComponent implements OnInit {
+
   @Output() columnsCountChange = new EventEmitter<number>()
+  @Output() itemsCountChange = new EventEmitter<number>()
+  @Output() sortChange = new EventEmitter<string>()
 
   sort = 'desc';
   itemsShowCount = 12;
@@ -18,12 +21,14 @@ export class ProductsHeaderComponent implements OnInit {
 
   onSortUpdated(newSort: string): void {
 
-    this.sort = newSort
+    this.sort = newSort;
+    this.sortChange.emit(newSort);
   }
 
   onItemsUpdated(count: number): void {
 
-    this.itemsShowCount = count
+    this.itemsShowCount = count;
+    this.itemsCountChange.emit(count)
   }
 
   onColumsUpdated(colsNum: number): void {
